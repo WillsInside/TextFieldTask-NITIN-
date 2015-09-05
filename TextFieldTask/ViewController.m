5f0413b520af800d26095b1e46f224007e43cb86
student <student@Achiever19.local>
2015-09-05 15:17:10 +0530

    Deleted the test slogs

--- TextFieldTask/ViewController.m
+++ TextFieldTask/ViewController.m
@@ -18,9 +18,6 @@
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
 
-    NSLog(@"Test");
-    NSLog(@"Testing Branch");
-    NSLog(@"Final");
 
 }
 
